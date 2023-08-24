8b94471e078107d93cb144ad1edbc1c224c26e53
the-dmitry <dzmitrygaponenko@gmail.com>
2023-08-24 17:57:00 +0300

    feat: add new projects

--- projects.js
+++ projects.js
@@ -1,35 +1,44 @@
 const projects = [
   {
-    name: 'Travel',
-    src: 'image/projects/Travel.png',
-    link: `https://the-dmitry.github.io/Travel/`
+    name: "Virtual Keyboard",
+    src: "image/projects/virtual-keyboard.png",
+    link: `https://the-dmitry.github.io/virtual-keyboard/`,
   },
   {
-    name: 'Online-zoo',
-    src: 'image/projects/online-zoo.png',
-    link: `https://the-dmitry.github.io/online-zoo/zoo/main.html`
+    name: "Online-zoo",
+    src: "image/projects/online-zoo.png",
+    link: `https://the-dmitry.github.io/online-zoo/zoo/main.html`,
   },
   {
-    name: 'Gem Puzzle',
-    src: 'image/projects/puzzle.png',
-    link: 'https://the-dmitry.github.io/puzzle/'
+    name: "Gem Puzzle",
+    src: "image/projects/puzzle.png",
+    link: "https://the-dmitry.github.io/puzzle/",
   },
   {
-    name: 'Momentum',
-    src: 'image/projects/momentum.png',
-    link: 'https://the-dmitry.github.io/Mometum-RS/'
+    name: "Momentum",
+    src: "image/projects/momentum.png",
+    link: "https://the-dmitry.github.io/Momentum/",
+  },
+  // {
+  //   name: "SongBird",
+  //   src: "image/projects/songbird.png",
+  //   link: "https://the-dmitry-songbird.netlify.app/",
+  // },
+  {
+    name: "Plants",
+    src: "image/projects/Plants.png",
+    link: "https://the-dmitry.github.io/Plants/",
   },
   {
-    name: 'SongBird',
-    src: 'image/projects/songbird.png',
-    link: 'https://the-dmitry-songbird.netlify.app/'
+    name: "Shelter",
+    src: "image/projects/shelter.png",
+    link: "https://the-dmitry.github.io/Shelter/main",
   },
   {
-    name: 'Plants',
-    src: 'image/projects/Plants.png',
-    link: 'https://the-dmitry.github.io/Plants/'
+    name: "Minesweeper",
+    src: "image/projects/minesweeper.png",
+    link: "https://the-dmitry.github.io/Minesweeper/",
   },
-]
+];
 
-
-export default projects
+export default projects;
